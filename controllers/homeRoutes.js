e2d28f6db38b352c94d055abeb037500d823367b
Hayden Dillon-Lloyd <115908599+rayjohn666@users.noreply.github.com>
2023-03-19 23:00:23 -0700

    changed User to user on homeroutes.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const User = require("../models/User");
+const User = require("../models/user");
 const router = express.Router();
 // make sure to use this format  for const router
 const bcrypt = require("bcrypt");
