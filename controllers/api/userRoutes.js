8af3a4397d80881430f99af11307ce0e9518543b
Hayden Dillon-Lloyd <115908599+rayjohn666@users.noreply.github.com>
2023-04-02 12:35:05 -0700

    cleared out old code, added console.log

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,44 +1,9 @@
 const { User, Comment, Post } = require("../../models");
 const router = require("express").Router();
-// make sure to use this format  for const router
 const bcrypt = require("bcrypt");
 var session = require("express-session");
 
-// router.get("/", (req, res) => {
-//   User.findAll({
-//     include: [Post, Comment],
-//   })
-//     .then((dbUser) => {
-//       res.json(dbUser);
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-// });
 
-// router.get("/signup", (req, res) => {
-//   res.render("signup", { layout: "main", logged_in: req.session.logged_in });
-// });
-
-// router.post("/login", (req, res) => {
-//   User.findOne({
-//     where: {
-//       username: req.body.username,
-//     },
-//   }).then((username) => {
-//     res.json(username);
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
-// });
-
-// router.get("/disclaimer", (req, res) => {
-//   res.render("disclaimer", {
-//     layout: "main",
-//     logged_in: req.session.logged_in,
-//   });
-// });
 
 
 router.post('/', async (req, res) => {
@@ -57,27 +22,6 @@ router.post('/', async (req, res) => {
   }
 });
 
-// router.post("/", async (req, res) => {
-//   try {
-//     const dbUserData = await User.create({
-//       username: req.body.username,
-//       email: req.body.email,
-//       password: req.body.password,
-//     });
-
-//     req.session.save(() => {
-//       req.session.logged_in = true;
-
-//       res.render("homepage", {
-//         layout: "main",
-//         logged_in: req.session.logged_in,
-//       });
-//     });
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json(err);
-//   }
-// });
 
 router.post("/login", async (req, res) => {
   try {
